95e6cbd1a5e693caec3e0bacaeedd541a3dc9f01
kKyy14 <pratamadhika141@gmail.com>
2025-10-24 19:19:26 +0700

    commit 1

---
+++ DisplayTwo07.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DisplayTwo07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numInput;
+        System.out.print("Input some number: ");
+        numInput = input.nextInt();
+
+        for(int i = 2; i <= numInput ; i += 2) {
+            System.out.print(" " + i);
+        }   
+
+        System.out.print(" %");
+    }
+}
